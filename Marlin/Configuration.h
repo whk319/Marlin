f90d8c59b6695b39257c5f6c6ec5a41fb6f4f7a5
whk319 <herb.hhr@gmail.com>
2021-07-23 13:06:37 +1000

    Update Configuration.h
    remove bltouch

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -23,7 +23,7 @@
 
 #define CONFIG_EXAMPLES_DIR "AnyCubic/i3 Mega"
 
-#define I3MEGA_HAS_BLTOUCH
+//#define I3MEGA_HAS_BLTOUCH
 #define I3MEGA_HAS_TMC2208
 
 /**
@@ -74,7 +74,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Herb, default with TMC2208 + BLTOUCH)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Herb, default)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
